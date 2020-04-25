f63f15439cf4979d7beaf353602641bfd2b5d174
Antonio Espinoza <aespinoza777@gmail.com>
2020-04-25 20:03:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,3 +13,20 @@ def sort_array_char_count(array)
     left.length <=> right.length
   end
 end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |item|
+    item[2] = "$"
+  end
+end
+
+
